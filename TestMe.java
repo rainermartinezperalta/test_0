4b9ed3a0d3a6ddadd9ff38cb459d74e33662b6ef
rainermartinezperalta <rainer.martinez-peralta@uconn.edu>
2024-09-17 20:58:23 -0400

    Update TestMe.java

--- TestMe.java
+++ TestMe.java
@@ -1,17 +1,19 @@
 package c;
 
 public class TestMe {
-	private double value = Double.NaN;
+    private double value = Double.NaN;
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 
-	public TestMe() {}
+    public TestMe() {}
 
-	public double square() { return value*value; }
-	public void setValue(double v) {
-		value = v;
-	}
+    public double square() {
+        return value * value;
+    }
 
+    public void setValue(double v) {
+        value = v;
+    }
 }
