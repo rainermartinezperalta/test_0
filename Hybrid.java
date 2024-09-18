2c33d5efdca19e6fb21212430f35f5f0bca8089a
rainermartinezperalta <rainer.martinez-peralta@uconn.edu>
2024-09-17 20:54:09 -0400

    Update Hybrid.java

--- Hybrid.java
+++ Hybrid.java
@@ -1,37 +1,42 @@
 public class Hybrid implements ElectricInterface, GasolineInterface {
 
     private double gasMPG;
-    private double MpgE;
+    private double MPGe;
     private double miles;
     private double gallons;
     private double kWh;
 
     private final double MPG_E = 33.7;
 
+    @Override
     public double calcGasMPG() {
-        gasMPG = miles/gallons;
+        gasMPG = miles / gallons;
         return gasMPG;
     }
 
+    @Override
     public double calcMPGe() {
-        MpgE = (miles/kWh) * MPG_E;
-        return  MpgE;
+        MPGe = (miles / kWh) * MPG_E;
+        return MPGe;
     }
 
+    @Override
     public void setMilesfromGas(double miles) {
         this.miles = miles;
     }
 
+    @Override
     public void setGallonsfromGas(double gallons) {
         this.gallons = gallons;
     }
 
+    @Override
     public void setElectricMiles(double totalElectricMiles) {
-        miles =  totalElectricMiles;
+        this.miles = totalElectricMiles;
     }
 
+    @Override
     public void setTotalkWh(double totalkWh) {
-        kWh = totalkWh;
+        this.kWh = totalkWh;
     }
-
 }
